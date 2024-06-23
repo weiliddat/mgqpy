2e440771a96bd76f3ee1db5789fb0fc16d182832
chiawei <chiawei.ong@parcellab.com>
2024-06-23 06:29:59 +0200

    feat: $lte

--- mgqpy/__init__.py
+++ mgqpy/__init__.py
@@ -69,6 +69,8 @@ class Query:
                     results.append(_match_gte(doc, path_parts, exp_or_ov["$gte"]))
                 if "$lt" in exp_or_ov:
                     results.append(_match_lt(doc, path_parts, exp_or_ov["$lt"]))
+                if "$lte" in exp_or_ov:
+                    results.append(_match_lte(doc, path_parts, exp_or_ov["$lte"]))
             else:
                 results.append(_match_eq(doc, path_parts, exp_or_ov))
 
@@ -248,5 +250,62 @@ def _match_lt(doc, path: List[str], ov) -> bool:
 
     if isinstance(doc, list):
         return any([_match_lt(d, path, ov) for d in doc])
+
+    return False
+
+
+def _match_lte(doc, path: List[str], ov) -> bool:
+    if len(path) == 0:
+        if isinstance(doc, list) and any([_match_lte(d, path, ov) for d in doc]):
+            return True
+
+        if isinstance(doc, list) and isinstance(ov, list):
+            if doc <= ov:
+                return True
+
+        if isinstance(doc, dict) and isinstance(ov, dict):
+            if not doc and not ov:
+                return True
+
+            keys = zip_longest(doc.keys(), ov.keys())
+            for doc_key, ov_key in keys:
+                if doc_key is None:
+                    return True
+                if ov_key is None:
+                    return False
+                if doc_key != ov_key:
+                    return doc_key < ov_key
+                if doc_key == ov_key:
+                    if doc[doc_key] > ov[ov_key]:
+                        return False
+            return True
+
+        if isinstance(doc, Number) and isinstance(ov, Number):
+            return operator.le(doc, ov)
+
+        if isinstance(doc, str) and isinstance(ov, str):
+            return operator.le(doc, ov)
+
+        if doc is None and ov is None:
+            return True
+
+        return False
+
+    key = path[0]
+    rest = path[1:]
+
+    if isinstance(doc, dict) and key in doc:
+        return _match_lte(doc[key], rest, ov)
+
+    if isinstance(doc, list) and key.isdigit():
+        idx = int(key)
+        if idx < len(doc):
+            return _match_lte(doc[idx], rest, ov)
+
+    if isinstance(doc, list):
+        return any([_match_lte(d, path, ov) for d in doc])
+
+    if ov is None:
+        return True
 
     return False
---
+++ tests/test_lte.py
@@ -0,0 +1,257 @@
+import copy
+
+import pytest
+
+from mgqpy import Query
+
+testcases = [
+    (
+        "$lte number",
+        {"foo": {"$lte": 1}},
+        [
+            {"foo": 0},
+            {"foo": 1},
+            {"foo": 2},
+            {"foo": "0"},
+            {"foo": "1"},
+            {"foo": "2"},
+            {},
+            {"foo": "baz"},
+            {"foo": {"foo": "bar"}},
+            {"foo": None},
+        ],
+        [
+            {"foo": 0},
+            {"foo": 1},
+        ],
+    ),
+    (
+        "$lte str",
+        {"foo": {"$lte": "bar"}},
+        [
+            {"foo": 0},
+            {"foo": 1},
+            {"foo": 2},
+            {},
+            {"foo": "baa"},
+            {"foo": "bar"},
+            {"foo": "baz"},
+            {"foo": {"foo": "bar"}},
+            {"foo": None},
+        ],
+        [
+            {"foo": "baa"},
+            {"foo": "bar"},
+        ],
+    ),
+    (
+        "$lte str",
+        {"foo": {"$lte": "1"}},
+        [
+            {"foo": 0},
+            {"foo": 1},
+            {"foo": 2},
+            {},
+            {"foo": "0"},
+            {"foo": "1"},
+            {"foo": "baa"},
+            {"foo": "bar"},
+            {"foo": "baz"},
+            {"foo": {"foo": "bar"}},
+            {"foo": None},
+        ],
+        [
+            {"foo": "0"},
+            {"foo": "1"},
+        ],
+    ),
+    (
+        "$lte none",
+        {"foo": {"$lte": None}},
+        [
+            {"foo": -1},
+            {"foo": 0},
+            {"foo": 1},
+            {},
+            {"foo": "baz"},
+            {"foo": {"foo": "bar"}},
+            {"foo": None},
+        ],
+        [
+            {},
+            {"foo": None},
+        ],
+    ),
+    (
+        "nested object path, $lte number",
+        {"foo.bar": {"$lte": 1}},
+        [
+            {"foo": {"bar": 0}},
+            {"foo": {"bar": 1}},
+            {"foo": {"bar": 2}},
+            {},
+            {"foo": {"bar": "baz"}},
+            {"foo": None},
+        ],
+        [
+            {"foo": {"bar": 0}},
+            {"foo": {"bar": 1}},
+        ],
+    ),
+    (
+        "nested object path, $lte str",
+        {"foo.bar": {"$lte": "baj"}},
+        [
+            {"foo": {"bar": 0}},
+            {"foo": {"bar": 1}},
+            {"foo": {"bar": 2}},
+            {},
+            {"foo": {"bar": "baa"}},
+            {"foo": {"bar": "baj"}},
+            {"foo": {"bar": "baz"}},
+            {"foo": None},
+        ],
+        [
+            {"foo": {"bar": "baa"}},
+            {"foo": {"bar": "baj"}},
+        ],
+    ),
+    (
+        "nested object path, $lte None",
+        {"foo.bar": {"$lte": None}},
+        [
+            {"foo": {"bar": {}}},
+            {"foo": {"bar": []}},
+            {"foo": {"bar": -1}},
+            {"foo": {"bar": 0}},
+            {"foo": {"bar": 1}},
+            {},
+            {"foo": {"bar": "baz"}},
+            {"foo": None},
+        ],
+        [
+            {},
+            {"foo": None},
+        ],
+    ),
+    (
+        "intermediate indexed array, $lte number",
+        {"foo.1.bar": {"$lte": 1}},
+        [
+            {"foo": [{}, {"bar": 0}]},
+            {"foo": [{"bar": 2}, {}]},
+            {},
+            {"foo": "bar"},
+            {"foo": [{"bar": "baz"}]},
+            {"foo": None},
+        ],
+        [
+            {"foo": [{}, {"bar": 0}]},
+        ],
+    ),
+    (
+        "doc leaf array, $lte number",
+        {"foo.bar": {"$lte": 1}},
+        [
+            {"foo": {"bar": [0, -1]}},
+            {"foo": {"bar": [2, 1]}},
+            {"foo": {"bar": [2, 3]}},
+            {},
+            {"foo": "bar"},
+            {"foo": {"bar": "baz"}},
+            {"foo": None},
+        ],
+        [
+            {"foo": {"bar": [0, -1]}},
+            {"foo": {"bar": [2, 1]}},
+        ],
+    ),
+    (
+        "unindexed nested object path with intermediate arrays on doc",
+        {"a.b.c": {"$lte": 1}},
+        [
+            {"a": [{"b": [{"c": 0}]}]},
+            {"a": [{"b": [{"c": [1]}]}]},
+            {"a": [{"b": [{"c": 2}]}]},
+            {},
+            {"a": {"b": "bar"}},
+            {"a": None},
+        ],
+        [
+            {"a": [{"b": [{"c": 0}]}]},
+            {"a": [{"b": [{"c": [1]}]}]},
+        ],
+    ),
+    (
+        "nested object path, object comparison",
+        {"foo.bar": {"$lte": {"baz": "qux"}}},
+        [
+            {"foo": {"bar": {}}},
+            {"foo": {"bar": {"baa": "zap"}}},
+            {"foo": {"bar": {"baz": "bux"}}},
+            {"foo": {"bar": {"baz": "qux"}}},
+            {"foo": {"bar": {"baz": "zap"}}},
+            {"foo": {"bar": {"bla": "jaz"}}},
+            {"foo": {"bar": {"baz": "qux", "bla": "jaz"}}},
+            {},
+            {"foo": "bar"},
+            {"foo": {"bar": "baz"}},
+        ],
+        [
+            {"foo": {"bar": {}}},
+            {"foo": {"bar": {"baa": "zap"}}},
+            {"foo": {"bar": {"baz": "bux"}}},
+            {"foo": {"bar": {"baz": "qux"}}},
+        ],
+    ),
+    (
+        "nested object path, object comparison empty ov",
+        {"foo.bar": {"$lte": {}}},
+        [
+            {"foo": {"bar": {}}},
+            {"foo": {"bar": None}},
+            {"foo": {"bar": 1}},
+            {"foo": {"bar": "baz"}},
+            {"foo": {"bar": {"baz": "qux"}}},
+            {"foo": {"bar": {"baz": "qux", "bla": "jaz"}}},
+            {},
+            {"foo": "bar"},
+        ],
+        [
+            {"foo": {"bar": {}}},
+        ],
+    ),
+    (
+        "nested object path, list comparison",
+        {"foo.bar": {"$lte": ["bar", "baz"]}},
+        [
+            {"foo": {"bar": ["bar"]}},
+            {"foo": {"bar": ["zzz"]}},
+            {"foo": {"bar": ["bar", "baz"]}},
+            {"foo": {"bar": ["baz", "bar"]}},
+            {"foo": {"bar": ["baa", "bzz"]}},
+            {"foo": {"bar": ["bzz", "baa"]}},
+            {"foo": {"bar": ["bar", "baz", "qux"]}},
+            {},
+            {"foo": "bar"},
+            {"foo": {"bar": "baz"}},
+        ],
+        [
+            {"foo": {"bar": ["bar"]}},
+            {"foo": {"bar": ["bar", "baz"]}},
+            {"foo": {"bar": ["baa", "bzz"]}},
+        ],
+    ),
+]
+
+
+@pytest.mark.parametrize("name,query,input,expected", testcases)
+def test_mgqpy_lte(test_db, name, query, input, expected):
+    q = Query(query)
+
+    test_db.insert_many(copy.deepcopy(input))
+    mongo_expected = test_db.find(q._query, projection={"_id": False})
+    assert list(mongo_expected) == expected, name
+
+    actual = filter(q.match, input)
+    assert list(actual) == expected, name
